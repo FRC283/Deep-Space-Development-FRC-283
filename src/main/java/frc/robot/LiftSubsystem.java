08fe0dfbf3e045fa6a7462b483e7e4ec4fd249f3
Christian Holcombe <christian220h@outlook.com>
2019-02-16 12:46:14 -0500

    Initial Commit

--- src/main/java/frc/robot/LiftSubsystem.java
+++ src/main/java/frc/robot/LiftSubsystem.java
@@ -1,11 +1,15 @@
 package frc.robot;
 
-
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class LiftSubsystem
 {
+    boolean upDown = false;
+    
+    Solenoid leftFront, rightFront, leftBack, rightBack;
+    Solenoid[] lift;
     LiftSubsystem()
     {
-        
+        lift = new Solenoid[] {leftFront, rightFront, leftBack, rightBack}; 
     }
 }
