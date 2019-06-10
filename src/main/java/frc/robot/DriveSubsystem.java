a4c9a9bd86d19b17b1af8962d31a9f148e679312
Ben Ranson <benr444@bellsouth.net>
2019-06-10 19:55:54 -0400

    All changes before Laptop B wipe

--- src/main/java/frc/robot/DriveSubsystem.java
+++ src/main/java/frc/robot/DriveSubsystem.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveSubsystem
 {
-    TalonSRX frontLeftDrive, backLeftDrive, frontRightDrive, backRightDrive; 
-
+    double driveCoefficient = 1.0;
+    TalonSRX frontLeftDrive, backLeftDrive, frontRightDrive, backRightDrive;
     Utilities283 utils;
     DriveSubsystem()
     {
@@ -22,10 +22,10 @@ public class DriveSubsystem
         frontRightDrive.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
         backRightDrive.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
 
-        frontLeftDrive.configOpenloopRamp(0.25);
-        backLeftDrive.configOpenloopRamp(0.25);
-        frontRightDrive.configOpenloopRamp(0.25);
-        backRightDrive.configOpenloopRamp(0.25);
+        frontLeftDrive.configOpenloopRamp(0);
+        backLeftDrive.configOpenloopRamp(0);
+        frontRightDrive.configOpenloopRamp(0);
+        backRightDrive.configOpenloopRamp(0);
     }
     /**
      * Function for controlling the motor controllers on the robot.
@@ -38,10 +38,10 @@ public class DriveSubsystem
      */
     public void drive(double lYMag, double lXMag, double rXMag)
     {
-        frontLeftDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (rXMag - lYMag + lXMag) * 1));		
-        frontRightDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (-rXMag - lYMag - lXMag) * -1));
-		backLeftDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (-rXMag + lYMag + lXMag) * -1));
-        backRightDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (rXMag + lYMag - lXMag) * 1));
+        frontLeftDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (rXMag - lYMag + lXMag) * (1 * driveCoefficient)));		
+        frontRightDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (-rXMag - lYMag - lXMag) * (-1 * driveCoefficient)));
+		backLeftDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (-rXMag + lYMag + lXMag) * (-1 * driveCoefficient)));
+        backRightDrive.set(ControlMode.PercentOutput, Utilities283.rescale(Constants.DEAD_ZONE, 1.0, 0.0, 1.0, (rXMag + lYMag - lXMag) * (1 * driveCoefficient)));
     }
 
     /**Sets SmartDashboard values for DriveSubsystem periodically */
@@ -55,6 +55,16 @@ public class DriveSubsystem
         SmartDashboard.putNumber("BackLeftDrive Encoder Velocity", backLeftDrive.getSelectedSensorVelocity(0));
         SmartDashboard.putNumber("FrontRightDrive Encoder Velocity", frontRightDrive.getSelectedSensorVelocity(0));
         SmartDashboard.putNumber("BackRightDrive Encoder Velocity", backRightDrive.getSelectedSensorVelocity(0));
+        //SmartDashboard.putNumber("FrontLeftDrive Magnitude", frontLeftDrive.get());
+        //SmartDashboard.putNumber("BackLeftDrive Magnitude", backLeftDrive.get());
+        //SmartDashboard.putNumber("FrontRightDrive Magnitude", frontRightDrive.get());
+        //SmartDashboard.putNumber("BackRightDrive Magnitude", backRightDrive;
+        
+    }
+
+    public void setDriveCoefficient(double val)
+    {
+        driveCoefficient = val;
     }
 
     public void resetter()
